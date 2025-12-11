f33f1a5e2b7cda0277c972435ae7b40522b7aeb9
YSH-NYK <yashnaik7664@gmail.com>
2025-12-12 00:36:36 +0530

    Responsive Hero and Featured Games

--- Frontend/my-app/src/pages/Home.tsx
+++ Frontend/my-app/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import HeroSection from '@/components/HeroSection'
 import Games from '@/components/Games'
 import React from 'react'
+import Footer from '@/components/Footer'
 
 const Home = () => {
   return (
     <div id='home'>
     <HeroSection/>
     <Games/>
+    <Footer/>
     </div>
   )
 }
