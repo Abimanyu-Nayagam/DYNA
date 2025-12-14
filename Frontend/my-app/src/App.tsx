0f8867fe3ed7a73bb5c757ebbcf0d85b106e03c3
Deepti-Velip <deeptivelip7@gmail.com>
2025-12-14 10:13:17 +0530

    add csgo page and  create form

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -7,6 +7,8 @@ import { Login } from "./pages/Login";
 import { AuthProvider } from "./contexts/AuthProvider";
 import PubgPlayersPage from "./pages/PubgPlayersPage";
 import CreatePubgPortfolioPage from "./pages/CreatePubgPortfolioPage";
+import CsgoPlayersPage from "./pages/CsgoPlayersPage";
+import CreateCsgoPortfolioPage from "./pages/CreateCsgoPortfolioPage";
 
 function App() {
   return (
@@ -19,9 +21,14 @@ function App() {
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/players/pubg" element={<PubgPlayersPage />} />
+            <Route path="/players/csgo" element={<CsgoPlayersPage />} />
             <Route
               path="/players/pubg/create"
               element={<CreatePubgPortfolioPage />}
+            />
+            <Route
+              path="/players/csgo/create"
+              element={<CreateCsgoPortfolioPage />}
             />
           </Routes>
         </AuthProvider>
