d0366d4cea54d53b764c17aaf7701425df4b9fd2
Nash-69 <nashvaz40@gmail.com>
2025-12-14 17:42:10 +0530

    added requirements and some mising final and a config.py

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -1,29 +1,27 @@
 import os
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 load_dotenv()
 
 class Config:
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_NAME = os.getenv('DB_NAME')
-    DB_PORT = os.getenv('DB_PORT', '3306')
 
+    LOG_FILE = os.getenv('LOG_FILE',"logs/app.log")
+    LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # DB_USER = os.getenv('DB_USER')
+    # DB_PASSWORD = os.getenv('DB_PASSWORD')
+    # DB_HOST = os.getenv('DB_HOST')
+    # DB_NAME = os.getenv('DB_NAME')
+    # DB_PORT = os.getenv('DB_PORT', '3306')
+
+    # encoded_password = quote_plus(DB_PASSWORD)
     # SQLALCHEMY_DATABASE_URI = (
     #     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     #     )
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY','secret-key')
-    LOG_FILE = os.getenv('LOG_FILE')
-    LOG_LEVEL='INFO'
-
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-    GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
-    GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
-    GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
-    GITHUB_REDIRECT_URI = os.getenv("GITHUB_REDIRECT_URI")
-
