b2769159ea4b332f5db81ef92ed0d39dab4aa2ed
Nash-69 <nashvaz40@gmail.com>
2025-12-17 01:42:06 +0530

    removed some duplicates from config

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -19,16 +19,5 @@ class Config:
         )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # DB_USER = os.getenv('DB_USER')
-    # DB_PASSWORD = os.getenv('DB_PASSWORD')
-    # DB_HOST = os.getenv('DB_HOST')
-    # DB_NAME = os.getenv('DB_NAME')
-    # DB_PORT = os.getenv('DB_PORT', '3306')
-
-    # encoded_password = quote_plus(DB_PASSWORD)
-    # SQLALCHEMY_DATABASE_URI = (
-    #     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    #     )
-
 
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY','secret-key')
